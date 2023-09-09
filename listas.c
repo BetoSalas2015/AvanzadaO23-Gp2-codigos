5dd970fba3e554a4e5265dbb13d743661e12a658
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2023-09-08 18:42:39 -0600

    Sesión 12 - 08/09/2023

--- listas.c
+++ listas.c
@@ -41,6 +41,46 @@ void insert(char dato)
 	}
 }
 
+void insert1(char dato)
+{
+	nodo *nuevo = NULL;
+	nuevo = (nodo *) malloc( sizeof( nodo ) );
+	if(nuevo == NULL)
+	{
+		printf("No hay memoria. \n");
+		exit(1);
+	}
+	nuevo -> info = dato;
+	nuevo -> sig = NULL;
+
+	nuevo ->sig = raiz;
+	raiz = nuevo;
+}
+
+void insertn(char dato, int n)
+{
+	nodo *nuevo = NULL;
+	nuevo = (nodo *) malloc( sizeof( nodo ) );
+	if(nuevo == NULL)
+	{
+		printf("No hay memoria. \n");
+		exit(1);
+	}
+	nuevo -> info = dato;
+	nuevo -> sig = NULL;
+	if( raiz == NULL )
+	{
+		raiz = nuevo;
+	}
+	else
+	{
+		nodo *recorre = raiz;
+		while( recorre ->sig != NULL)
+			recorre = recorre ->sig; 
+		recorre -> sig = nuevo;
+	}
+}
+
 void imprimeLista()
 {
 	nodo *recorre = raiz;
@@ -56,13 +96,13 @@ int main()
 {
 	imprimeLista();
 
-	insert('R');
-	insert('o');
-	insert('b');
-	insert('e');
-	insert('r');
-	insert('t');
-	insert('o');
+	insert1('R');
+	insert1('o');
+	insert1('b');
+	insert1('e');
+	insert1('r');
+	insert1('t');
+	insert1('o');
 
 	imprimeLista();
 
